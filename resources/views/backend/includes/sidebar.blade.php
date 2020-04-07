811d6af4ce322c15da3bb1f69198c474cb5e0e93
yannikmaassen <yannik.maassen@t-online.de>
2020-04-07 19:31:23 +0200

    Add templates and routes

---
+++ resources/views/backend/includes/sidebar.blade.php
@@ -0,0 +1,20 @@
+<div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+<aside class="app-sidebar">
+  <ul class="app-menu">
+    <li>
+      <a class="app-menu__item active" href="index-2.html"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a>
+    </li>
+    <li>
+      <a class="app-menu__item" href="index-2.html"><i class="app-menu__icon fa fa-cube"></i><span class="app-menu__label">Products</span></a>
+    </li>
+    <li>
+      <a class="app-menu__item" href="index-2.html"><i class="app-menu__icon fa fa-files-o"></i><span class="app-menu__label">Categories</span></a>
+    </li>
+    <li>
+      <a class="app-menu__item" href="index-2.html"><i class="app-menu__icon fa fa-envelope-o"></i><span class="app-menu__label">Orders</span></a>
+    </li>
+    <li>
+      <a class="app-menu__item" href="index-2.html"><i class="app-menu__icon fa fa-user-o"></i><span class="app-menu__label">Users</span></a>
+    </li>
+  </ul>
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend/home', [
+        'products' => App\Product::take(4)->get(),
+        'categories' => App\Category::all(),
+    ]);
 });
+
+Route::get('/admin', function () {
+    return view('backend/home', [
+        'productsCount' => App\Product::count(),
+        'categoriesCount' => App\Category::count(),
+    ]);
+});
+
+Route::get('/products', function () {
+    return view('frontend/products', [
+        'products' => App\Product::all(),
+    ]);
+});
+
+
+
+// Route::get('/products', function () use ($products) {
+//     return view('products', ['products' => $products]);
+// });
+
+// Route::get('/products/{id}', function ($id) use ($products) {
+//     if (array_key_exists($id, $products)) {
+//         return $products[$id];
+//     }
+
+//     abort(404, 'Product not found');
+// });
